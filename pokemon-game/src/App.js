39c85afae0c58627362182ee6efccddca3bdf57c
volha <v.khviadchenia@gmail.com>
2021-09-18 01:40:09 +0200

    feat: add component Layout

--- pokemon-game/src/App.js
+++ pokemon-game/src/App.js
@@ -1,14 +1,33 @@
 import React from "react";
+
+import FirstBg from './assets/bg1.jpg';
+import SecondBg from './assets/bg3.jpg';
+
 import Header from './components/Header/Header.js';
+import Layout from './components/Layout/Layout.js';
 
 const App = () => {
   return (
     <>
-      <Header title descr />
-      {/* <Layout />
-      <Layout />
-      <Layout />
-      <Footer /> */}
+      <Header
+        title="This is title"
+        descr="This is Description!"
+      />
+      <Layout
+        title="This is title"
+        descr="This is Description!"
+        urlBg = { FirstBg }
+      />
+      <Layout
+        title="This is title"
+        descr="This is Description!"
+      />
+      <Layout
+        title="This is title"
+        descr="This is Description!"
+        urlBg={ SecondBg }
+      />
+      {/* <Footer /> */}
     </>
   );
 }
--- pokemon-game/src/components/Layout/Layout.js
+++ pokemon-game/src/components/Layout/Layout.js
@@ -1,11 +1,28 @@
 import React from "react";
 
-import l from './layout.css';
+import l from './layout.module.css';
 
-const Layout = () => {
+const Layout = ({ title, descr, urlBg, colorBg='#e2e2e2'}) => {
+  const rootBg = {
+        background: `${colorBg}${urlBg
+            ? ` url(${urlBg}) no-repeat fixed left bottom / cover`
+            : ''}`,
+    };
   return (
     <>
-
+      <section className={ l.root } style={ rootBg }>
+        <div className={ l.wrapper }>
+            <article>
+              <div className={ l.title }>
+                <h3>{ title }</h3>
+                <span className={ l.separator }></span>
+              </div>
+              <div className={l.descr+ ' ' + l.full}>
+                <p>{ descr }</p>
+              </div>
+            </article>
+        </div>
+      </section>
     </>
   )
 }
